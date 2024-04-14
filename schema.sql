daaa33e4d10842db326790b750bac3deb96f0243
MarchMol <marchmol98@gmail.com>
2024-04-13 18:11:59 -0600

    Fix: fixed syntax errors, added a field in table pedidos, added notation, and now everiting is inside a transaction

--- schema.sql
+++ schema.sql
@@ -1,26 +1,26 @@
 -- TABLAS Y LLAVES --
 
+BEGIN;
 -- Areas
 CREATE TABLE areas(
     id_area VARCHAR(10) PRIMARY KEY,
     nombre VARCHAR(50),
     fumadores BOOLEAN
 );
---
+-- \Areas
 
 -- Empleados
 CREATE TABLE empleados(
     id_empleado VARCHAR(10) PRIMARY KEY,
     nombre VARCHAR(50),
     rol VARCHAR(50),
-    area VARCHAR(10) NULL,
+    area VARCHAR(10) NULL --FK
 );
-
 ALTER TABLE empleados
 ADD CONSTRAINT fk_areas_empleados
 FOREIGN KEY (area)
 REFERENCES areas(id_area);
---
+-- \Empleados
 
 -- Menu
 CREATE TABLE menu(
@@ -29,14 +29,14 @@ CREATE TABLE menu(
     descripcion TEXT,
     precio DOUBLE PRECISION
 );
---
+--\Menu
 
 -- Mesas
 CREATE TABLE mesas(
     id_mesa VARCHAR(10) PRIMARY KEY,
     capacidad INT,
     movible BOOLEAN,
-    area VARCHAR(10)
+    area VARCHAR(10) --FK
 );
 
 ALTER TABLE mesas
@@ -48,8 +48,8 @@ REFERENCES areas(id_area);
 
 -- TABLA INTERMEDIA: mesas juntas
 CREATE TABLE mesas_juntas(
-    mesa_principal VARCHAR(10),
-    mesa_adicional VARCHAR(10)
+    mesa_principal VARCHAR(10), --FK
+    mesa_adicional VARCHAR(10) --FK
 );
 
 ALTER TABLE mesas_juntas
@@ -59,7 +59,7 @@ REFERENCES mesas(id_mesa),
 ADD CONSTRAINT fk_mesa_adicional
 FOREIGN KEY (mesa_adicional)
 REFERENCES mesas(id_mesa);
---
+-- \mesas juntas
 
 -- Cuentas
 CREATE TABLE cuentas(
@@ -67,37 +67,37 @@ CREATE TABLE cuentas(
     estado BOOLEAN,
     tiempo_abierta TIMESTAMP,
     tiempo_carrada TIMESTAMP,
-    mesa VARCHAR(10)
+    mesa VARCHAR(10) --FK
 );
 ALTER TABLE cuentas
 ADD CONSTRAINT fk_cuenta_mesa
 FOREIGN KEY (mesa)
 REFERENCES mesas(id_mesa);
---
+-- \Cuentas
 
 -- Encuesta
 CREATE TABLE encuestas(
     id_encuesta VARCHAR(10) PRIMARY KEY,
     amabilidad INT,
     exactitud INT,
-    cuenta VARCHAR(10),
+    cuenta VARCHAR(10), --FK
     fecha DATE
 );
 ALTER TABLE encuestas
 ADD CONSTRAINT fk_encuesta_cuenta
 FOREIGN KEY (cuenta)
 REFERENCES cuentas(id_cuenta);
---
+-- \Encuesta
 
--- Queja
+-- Quejas
 CREATE TABLE quejas(
     id_queja VARCHAR(10) PRIMARY KEY,
     nombre VARCHAR(50),
     fecha DATE,
     motivo TEXT,
     gravedad INT,
-    empleado VARCHAR(10) NULL,
-    alimento VARCHAR(10) NULL
+    empleado VARCHAR(10) NULL, --FK
+    alimento VARCHAR(10) NULL --FK
 );
 ALTER TABLE quejas
 ADD CONSTRAINT fk_queja_empleado
@@ -106,12 +106,13 @@ REFERENCES empleados(id_empleado),
 ADD CONSTRAINT fk_queja_alimento
 FOREIGN KEY (alimento)
 REFERENCES menu(id_alimento);
---
+-- \Quejas
 
 -- Pedidos
 CREATE TABLE pedidos(
-    cuenta VARCHAR(10),
-    alimento VARCHAR(10)
+    cuenta VARCHAR(10), --FK
+    alimento VARCHAR(10), --FK
+    tiempo TIMESTAMP
 );
 ALTER TABLE pedidos
 ADD CONSTRAINT fk_pedido_cuenta
@@ -120,7 +121,7 @@ REFERENCES cuentas(id_cuenta),
 ADD CONSTRAINT fk_pedido_alimento
 FOREIGN KEY (alimento)
 REFERENCES menu(id_alimento);
---
+-- \Pedidos
 
 -- Factura
 CREATE TABLE facturas(
@@ -137,4 +138,6 @@ ALTER TABLE facturas
 ADD CONSTRAINT fk_factura_cuenta
 FOREIGN KEY (cuenta)
 REFERENCES cuentas(id_cuenta);
---
+-- \Pedidos
+
+COMMIT;
